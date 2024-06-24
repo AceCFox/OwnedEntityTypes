32098c0e35078d571e16931a4598cccafe32dc0a
Ace Fox <afox09@gmail.com>
2024-06-24 09:12:54 -0500

    initial commit

---
+++ Entity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OwnedEntityTypes
+{
+    public class Entity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set;  }
+
+        public Entity() { 
+            Name = string.Empty;
+            Description = Name + "Entity";
+            Color = string.Empty;
+        }
+    }
+}
--- obj/Debug/net8.0/OwnedEntityTypes.AssemblyInfo.cs
+++ obj/Debug/net8.0/OwnedEntityTypes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OwnedEntityTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6f528fabb9f0783b6ace3c5fe8b23069dd6838")]
 [assembly: System.Reflection.AssemblyProductAttribute("OwnedEntityTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OwnedEntityTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
